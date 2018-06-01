e5634ca9a50314ad1a16fddb0e751c231cb8a595
maximeoger <maximeoger93@gmail.com>
2018-06-01 13:54:05 +0200

    fix

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,28 +1,39 @@
 class Story {
-  constructor(bg, data) {
-    this.bg = bg
-    this.data = data
+
+  constructor() {
+    this.getData();
   }
-  show() {
-    console.log(this.bg, this.data)
-  }
+
   getData() {
-    fetch('./src/assets/js/story.json')
-      .then((response) => response.json())
-      .then((data) => {
-        let output = '<h2>story</h2>';
-        data.forEach(function(data){
-          output += `
-            <ul>
-                <li>id: ${data.id}</li>
-                <li>situation: ${data.situation[0]}</li>
-            </ul>
-          `;
-        });
-        document.querySelector('.output').innerHTML = output;
-      })
+    document.querySelector('button').addEventListener('click', () => {
+      fetch('./src/assets/js/story.json')
+        .then((response) => response.json())
+        .then((data) => {
+          this.init(data);
+        })
+    })
   }
+
+  init(data) {
+    let output;
+    data.forEach(function(data){
+      output = `<p> ${data.situation[0]} </p>`;
+    });
+    document.querySelector('.output').innerHTML = output;
+  }
+
+  engine() {
+
+  }
+
+  chooseA(){
+    alert('a');
+  }
+
+  chooseB(){
+    alert('b');
+  }
+
 }
 
-var story = new Story('lol', 9)
-story.getData();
+var story = new Story();
