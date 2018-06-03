9bacbafd2af1a168701496479c0f5b20e34898e3
maximeoger <maximeoger93@gmail.com>
2018-06-03 13:51:25 +0200

    clearOutput and render methods 🚀🚀

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,6 +1,7 @@
 class Story {
 
   constructor() {
+    console.log(this)
    this.startGame();
   }
 
@@ -22,39 +23,45 @@ class Story {
     }.bind(this))
   }
 
+  clearOutput() {
+    document.querySelector('.Output').innerHTML = "";
+    return;
+  }
+
   // interprète les données récupérées via getData et les affiche dans le dom
   render(data, situation) {
     let output = document.querySelector('.Output');
     let cursor = 0;
     let statement;
-    let timer = setInterval( function () {
+    
+    let timer = setInterval( () => {
       statement = ` <p> ${ data[situation].text[cursor] } </p> `;
       output.innerHTML += statement;
       if(cursor === data[situation].text.length - 1){
         clearInterval(timer);
         if( data[situation].choices ){
           output.innerHTML += `
-            <button class="Output__btn"> ${data[situation].choices.a} </button>
-            <button class="Output__btn"> ${data[situation].choices.b} </button>
+            <button class="Output__btn" data-choice="a"> ${data[situation].choices.a.label} </button>
+            <button class="Output__btn" data-choice="b"> ${data[situation].choices.b.label} </button>
           `;
+          document.querySelector('.Output__btn[data-choice="a"]').addEventListener('click', () => {
+            console.log(this)
+            this.clearOutput();
+            return this.render(data, 2)
+          });
+          document.querySelector('.Output__btn[data-choice="b"]').addEventListener('click', () => {
+            console.log(this)
+            this.clearOutput();
+            return this.render(data, 3)
+          });
         }else{
           output.innerHTML += `<button class="Output__btn"> Suivant </button>`;
         }
       }
       cursor++;
-    }, 1000)
-    
+    }, 1000);
     document.querySelector('body').style.backgroundColor = data[0].color;
-  }
-
-  engine() {
-
-  }
-
-  // enregistre les conséquences des réponses à la fin de chaque situation
-  saveState() {
-    
   }
 }
 
-var story = new Story();
+let story = new Story();
